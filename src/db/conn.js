13cfb09a7c8d5ea789573b5a181c99fb3e2a5c78
Yash Tikkha <82872112+YashTikkha07@users.noreply.github.com>
2023-08-27 17:41:10 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,1 +1,23 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
+dotenv.config({ path: '../../.env' });
+
+const CONNECTION_URL = process.env.MONGODB_CONNECTION_URL || "mongodb+srv://yashgangwar:Yash12345@cluster0.vo961.mongodb.net/calculator?retryWrites=true&w=majority";
+
+console.log(CONNECTION_URL);
+
+if (!CONNECTION_URL) {
+    console.error("Error: MONGODB_CONNECTION_URL environment variable is not set.");
+} else {
+    mongoose.connect(CONNECTION_URL,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    ).then((res) => {
+        console.log("Hurray!! Successfully Connected to the database");
+    }).catch((err) => {
+        console.log("Connection failed to the database!!");
+    });
+}   
