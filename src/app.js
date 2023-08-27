f2b4c9f93b950f62265497f5c4759bc264d13c3f
Yash Tikkha <82872112+YashTikkha07@users.noreply.github.com>
2023-08-27 17:50:01 +0530

    minor fix

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+require("../src/db/conn");
+const router = require('../src/routers/calculator');
 
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(router)
+
+app.get('/', async (req, res) => {
+    const homepage = path.join(__dirname, 'index.html');
+    res.sendFile(homepage);
+})
+
+app.listen(port, () => {
+    console.log(`Server is live at port no. ${port}`);
+})
